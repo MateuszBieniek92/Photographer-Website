2d5fc834ff525062a9840d6cf1a69640b83539f2
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-04-26 10:15:38 +0200

    Changed size of photo, optymalization.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
+var js = require('gulp-sass');
 
 gulp.task('hint', function () {
     return gulp.src('js/*.js')
@@ -18,6 +19,14 @@ gulp.task('sass', function () {
             sourceComments: 'map'
         }))
         .pipe(gulp.dest('css'))
+
+    ///
+//    return gulp.src('js/*.js')
+//        .pipe(sass({
+//            outputStyle: 'compressed',
+//            sourceComments: 'map'
+//        }))
+//        .pipe(gulp.dest('js2'))
 });
 
 gulp.task('watch', function () {
